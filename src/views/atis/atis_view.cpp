b931368b20f74b354c1c3decc3acb69363df246e
Juha Teurokoski <teurokoski.juha@gmail.com>
2016-05-08 08:06:59 +0300

    items places calculated
    Now every runway name is calculated before drawing. Controller receive
    when both is selected.

--- src/views/atis/atis_view.cpp
+++ src/views/atis/atis_view.cpp
@@ -1,5 +1,7 @@
 #include "atis_view.hpp"
 
+Rwy::Rwy(std::string n, Point p1, Point p2) : name(n), place_lu(p1), place_rd(p2) { }
+
 Atisview::Atisview(Drawsurface& d, Settings& s) : View(d), settings(s) {
     this->load();
 }
@@ -14,18 +16,64 @@ void Atisview::draw() {
     View::draw();
 }
 
+std::string Atisview::get_dep_runway_name(Point& mouse) {
+	for (unsigned int i = 0; i < depa.size(); ++i) {
+		if (Tools::on_area(mouse, depa[i].place_lu, depa[i].place_rd)) {
+			this->selected_dep = i;
+			return depa[i].name;
+		}
+	}
+	
+	return "";
+}
+
+std::string Atisview::get_lnd_runway_name(Point& mouse) {
+	for (unsigned int i = 0; i < land.size(); ++i) {
+		if (Tools::on_area(mouse, land[i].place_lu, land[i].place_rd)) {
+			this->selected_lnd = i;
+			return land[i].name;
+		}
+	}
+	
+	return "";
+}
+
 void Atisview::draw_runways(std::list <std::string>& runways) {
+	this->calculate_places(runways);
+	std::string color;
+	
+	for (unsigned int i = 0; i < depa.size(); ++i) {
+		color = (i == this->selected_dep) ? "green" : "red";
+		drawer.draw_text(depa[i].name, depa[i].place_lu, color);
+	}
+	
+	for (unsigned int i = 0; i < land.size(); ++i) {
+		color = (i == this->selected_lnd) ? "green" : "red";
+		drawer.draw_text(land[i].name, land[i].place_lu, color);
+	}
+}
+
+void Atisview::calculate_places(std::list <std::string>& runways) {
+	this->depa.clear();
+	this->land.clear();
+	
 	Point p1(250, 150);
 	Point p2(450, 150);
+	
+	int width = 60;
+	int height = drawer.get_fontsize();
 	
 	drawer.draw_text("departure", p1, "red");
 	drawer.draw_text("landing", p2, "red");
 	
 	for (std::list<std::string>::iterator it = runways.begin(); it != runways.end(); ++it) {
-		p1.change_y(drawer.get_fontsize());
-		p2.change_y(drawer.get_fontsize());
+		p1.change_y(drawer.get_fontsize()+10);
+		p2.change_y(drawer.get_fontsize()+10);
 		
-		drawer.draw_text(*it, p1, "red");
-		drawer.draw_text(*it, p2, "red");
+		Point p1rd(p1.get_x() + width, p1.get_y() + height);
+		Point p2rd(p2.get_x() + width, p2.get_y() + height);
+		
+		depa.push_back(Rwy(*it, p1, p1rd));
+		land.push_back(Rwy(*it, p2, p2rd));
 	}
 }
--- src/views/atis/atis_view.hpp
+++ src/views/atis/atis_view.hpp
@@ -10,15 +10,32 @@
 #include "../../tools/settings.hpp"
 #include "../../models/atis.hpp"
 
+class Rwy {
+public:
+	Rwy(std::string n, Point p1, Point p2);
+	std::string name;
+	Point place_lu;
+	Point place_rd;
+};
+
 class Atisview : public View {
 public:
 	Atisview(Drawsurface& d, Settings& s);
 	~Atisview();
 	void draw();
 	void draw_runways(std::list <std::string>& runways);
+	void calculate_places(std::list <std::string>& runways);
+	std::string get_dep_runway_name(Point& mouse);
+	std::string get_lnd_runway_name(Point& mouse);
 private:
     Settings& settings;
     void load();
+	
+	std::vector <Rwy> land;
+	std::vector <Rwy> depa;
+	
+	unsigned int selected_dep;
+	unsigned int selected_lnd;
 };
 
 #endif // ATIS_VIEW
