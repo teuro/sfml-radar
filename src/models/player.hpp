9fc033a9d5cae5407029974761110014a83f4b69
Juha Teurokoski <teurokoski.juha@gmail.com>
2016-05-10 21:06:11 +0300

    Player basic data

--- src/models/player.hpp
+++ src/models/player.hpp
@@ -8,10 +8,26 @@ public:
     Player(Settings& s);
     ~Player();
     void update(double elapsed);
+	
+	void set_firstname(std::string firstname);
+	void set_surname(std::string surname);
+	void set_email(std::string email);
+	void set_username(std::string username);
+	
+	std::string get_firstname();
+	std::string get_surname();
+	std::string get_email();
+	std::string get_username();
 protected:
 private:
     Settings& settings;
+	
+	std::string firstname;
+	std::string surname;
+	std::string email;
+	std::string username;
+	std::string password_hash;
+	std::string password_salt;
 };
-
 
 #endif // PLAYER_HPP
