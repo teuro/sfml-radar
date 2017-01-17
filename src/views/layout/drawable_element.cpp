1c529408b20ea493a91aad3519a4006e0d1c81bb
Juha Teurokoski <teurokoski.juha@gmail.com>
2017-01-17 20:44:04 +0200

    default values

--- src/views/layout/drawable_element.cpp
+++ src/views/layout/drawable_element.cpp
@@ -1,22 +1,21 @@
 #include "drawable_element.hpp"
-Drawable_element::Drawable_element(std::string t_name, std::string t_class, std::string t_id) : name(t_name), s_class(t_class), id(t_id) { 
-	this->styles["text-color"] = 25648;
-	this->styles["background-color"] = -1;
-}
+Drawable_element::Drawable_element(std::string t_name, std::string t_class, std::string t_id) : name(t_name), s_class(t_class), id(t_id) { }
 
 Drawable_element::~Drawable_element() { }
 
 void Drawable_element::set_style(std::string key, int value) {
-	this->styles[key] = value;
+	if (value > 0 ) {
+		this->styles[key] = value;
+	}
 }
 
 void Drawable_element::set_style(Style& style) {
-	this->styles["left"] = style.get_place().get_x();
-	this->styles["top"] = style.get_place().get_y();
-	this->styles["width"] = style.get_width();
-	this->styles["height"] = style.get_height();
-	this->styles["text-color"] = style.get_text_color();
-	this->styles["background-color"] = style.get_background_color();
+	this->set_style("left", style.get_place().get_x());
+	this->set_style("top", style.get_place().get_y());
+	this->set_style("width", style.get_width());
+	this->set_style("height", style.get_height());
+	this->set_style("text-color", style.get_text_color());
+	this->set_style("background-color", style.get_background_color());
 }
 
 int Drawable_element::get_style(std::string key) {
