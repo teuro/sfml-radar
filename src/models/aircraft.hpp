d4bc3acbc87a0ccfab75ffc8b4bc8dc36af5e34e
Juha Teurokoski <teurokoski.juha@gmail.com>
2017-02-08 05:54:49 +0200

    cleanin and documentation

--- src/models/aircraft.hpp
+++ src/models/aircraft.hpp
@@ -12,19 +12,95 @@ class Aircraft {
 public:
     Aircraft(std::string name, double speed, double heading, double altitude, Coordinate p, int type, Settings& s, Runway& land, Navpoint& out);
     ~Aircraft();
-    void update(double elapsed);
-
+    
+	/**
+		* update
+		* Hndles aircrafts movement. 
+		* @param double elapsed time since previous update in milliseconds
+		* @return void
+	**/
+	
+	void update(double elapsed);
+	
+	/**
+		* get_place
+		* returns aircraft's current position in map coordinates 
+		* @param void
+		* @return Coordinate&
+	**/
+	
     Coordinate& get_place();
+	
+	/**
+		* get_target
+		* returns aircraft's current target point.
+		* @param void
+		* @return Navpoint&
+	**/
+	
 	Navpoint& get_target();
+	
+	/**
+		* get_name
+		* returns aircraft's name (callsign).
+		* @param void
+		* @return std::string
+	**/
+	
     std::string get_name();
-
+	
+	/**
+		* get_speed
+		* returns aircraft's current speed in knots.
+		* @param void
+		* @return double
+	**/
+	
     double get_speed();
+	
+	/**
+		* get_heading
+		* returns aircraft's current heading in radians.
+		* @param void
+		* @return double
+	**/
+	
     double get_heading();
+	
+	/**
+		* get_altitude
+		* returns aircraft's current altitude in feets.
+		* @param void
+		* @return double
+	**/
+	
     double get_altitude();
-    int get_turn();
-
+    
+	/**
+		* get_clearance_speed
+		* returns aircraft's current get_clearance_speed in knots.
+		* @param void
+		* @return double
+	**/
+	
     double get_clearance_speed();
+	
+	/**
+		* get_clearance_heading
+		* returns aircraft's current get_clearance_heading in radians.
+		* @param void
+		* @return double
+	**/
+	
     double get_clearance_heading();
+	
+	/**
+		* get_clearance_altitude
+		* returns aircraft's current get_clearance_altitude in feets.
+		* @param void
+		* @return double
+	**/
+	
     double get_clearance_altitude();
 	
 	void set_clearance_speed(double cl_spd);
@@ -60,7 +136,7 @@ private:
     bool approach;
     bool direct;
     bool landed;
-	bool check_approach_config(double target_altitude);
+	bool check_approach_config();
 	void calculate_angle_target(Coordinate& target);
     Settings& settings;
 	Navpoint target;
