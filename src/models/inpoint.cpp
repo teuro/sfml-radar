fa291c5f3f7676c1405c63cfb606572648326a3c
Juha Teurokoski <teurokoski.juha@gmail.com>
2017-02-19 19:58:56 +0200

    clean up

--- src/models/inpoint.cpp
+++ src/models/inpoint.cpp
@@ -1,16 +1,9 @@
 #include "inpoint.hpp"
 
-Inpoint::Inpoint(std::string name, Coordinate p, double spd, double alt, double hdg) : Navpoint(name, p), speed(spd), altitude(alt), heading(hdg) {
+Inpoint::Inpoint(std::string name, Coordinate p, double spd, double alt, double hdg) : Navpoint(name, p), speed(spd), altitude(alt), heading(hdg) { }
+Inpoint::Inpoint(std::string name, double latitude, double longitude, double spd, double alt, double hdg) : Navpoint(name, latitude, longitude), speed(spd), altitude(alt), heading(hdg) { }
 
-}
-
-Inpoint::Inpoint(std::string name, double latitude, double longitude, double spd, double alt, double hdg) : Navpoint(name, latitude, longitude), speed(spd), altitude(alt), heading(hdg) {
-
-}
-
-Inpoint::~Inpoint() {
-
-}
+Inpoint::~Inpoint() {}
 
 double Inpoint::get_altitude() {
     return this->altitude;
