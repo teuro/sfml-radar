bf878d6066b7417f4d37b072ab8808b792732323
Juha Teurokoski <teurokoski.juha@gmail.com>
2017-02-20 18:59:20 +0200

    semicolon support added

--- src/program.cpp
+++ src/program.cpp
@@ -93,7 +93,7 @@ bool Program::handle_event(sf::Event& event, Controller& ctrl, sf::RenderWindow&
             } else if (
 				((event.text.unicode >= 65 && event.text.unicode <= 122) ||
 				(event.text.unicode >= 48 && event.text.unicode <= 57) ||
-				(event.text.unicode == 32 || event.text.unicode == 58)) && 
+				(event.text.unicode == 32 || event.text.unicode == 59)) && 
 				event.text.unicode != 13 && event.text.unicode != 8) {
                 this->input_string += sf::String(event.text.unicode);
             }
